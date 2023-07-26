719b7b94f1e9552011edf44211527c91299e99c9
João Victor <jvctor23@gmail.com>
2023-07-26 13:57:13 -0300

    linked list v1

--- dataStructures/linkedList.py
+++ dataStructures/linkedList.py
@@ -1,3 +1,38 @@
 
 
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtBeginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+        print(self.head)
+
+    def print(self):
+        if self.head is None:
+            print("Empty")
+
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + '> '
+            itr = itr.next
+
+        print(llstr)
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    print(ll.head)
+    ll.insertAtBeginning(3)
+    ll.insertAtBeginning(12)
+    ll.print()
 
