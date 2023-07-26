dace4b392ada7243e84682e26c375d4453720480
João Victor <jvctor23@gmail.com>
2023-07-26 16:04:51 -0300

    linked list - remove at index

--- dataStructures/linkedList.py
+++ dataStructures/linkedList.py
@@ -22,6 +22,20 @@ class LinkedList:
             itr = itr.next
         itr.next = node
 
+    def removeAtIndex(self, index):
+        count = 0
+        itr = self.head
+        while itr.next:
+            if count == index-1:
+                break
+            else:
+                itr = itr.next
+            count += 1
+        deletedNode = itr.next
+        itr.next = deletedNode.next
+        print(f'{deletedNode.data} removido com sucesso')
+
+
     def display(self):
         if self.head is None:
             print("Empty")
@@ -44,8 +58,13 @@ class LinkedList:
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insertAtBeginning(10)
-    ll.insertAtBeginning(9)
+    ll.insertAtBeginning(1)
     ll.insertAtEnd(2)
+    ll.insertAtEnd(3)
+    ll.insertAtEnd(4)
+    ll.insertAtEnd(5)
+    ll.insertAtEnd(6)
+    ll.insertAtEnd(7)
+    ll.removeAtIndex(2)
     ll.display()
 
