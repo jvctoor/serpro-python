a9b0b0ec0eb4a065cb8e700e0d09db0dee834a9a
João Victor <jvctor23@gmail.com>
2023-08-07 18:21:39 -0300

    maxOperations2

--- problems/maxOperations.py
+++ problems/maxOperations.py
@@ -26,7 +26,22 @@ def maxOperations(nums, k):
     return operations
 
 
+def maxOperations2(nums, k):
+    mp = {}
+    operations = 0
+
+    for num in nums:
+        restante = k - num
+        if restante in mp and mp[restante] > 0:
+            operations += 1
+            mp[restante] -= 1
+        else:
+            mp[num] = mp.get(num, 0) + 1
+
+    return operations
+
+
 nums = [4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4]
 k = 2
 
-print(maxOperations(nums, k))
+print(maxOperations2(nums, k))
