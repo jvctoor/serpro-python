b47b0c41827212eec8976925f078d44b0014aaa4
João Victor <jvctor23@gmail.com>
2023-08-17 11:09:03 -0300

    Test

---
+++ tests/test_calc.py
@@ -0,0 +1,18 @@
+import unittest
+import problems.contadorVogais
+
+class TestCalc(unittest.TestCase):
+    def test_contar_vogais(self):
+        self.assertEqual(problems.contadorVogais.contarVogais("Joao"), 3)
+        self.assertEqual(problems.contadorVogais.contarVogais("Elefante"), 4)
+        self.assertEqual(problems.contadorVogais.contarVogais("Pai"), 2)
+        self.assertEqual(problems.contadorVogais.contarVogais("Cafe"), 2)
+        self.assertEqual(problems.contadorVogais.contarVogais("Dono"), 2)
+        self.assertEqual(problems.contadorVogais.contarVogais("Luana"), 3)
+
+
+
+
+
+
+
