64c5940f7e0692ec0a357d328f4b0ba746565b45
João Victor <jvctor23@gmail.com>
2023-08-20 17:53:28 -0300

    database conn

---
+++ files_sqlite/database.py
@@ -0,0 +1,16 @@
+import sqlite3
+from user_dao import *
+from sql_queries import *
+
+databaseName = "databasev1"
+
+with sqlite3.connect(databaseName) as connection:
+    cursor = connection.cursor()
+
+    dao = userDAO(connection)
+
+    print(dao.getUserById(1))
+
+
+
+
